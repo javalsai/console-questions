bfab2f7ffe57926f7360569d0e99533c49d70813
javalsai <80113062+javalsai@users.noreply.github.com>
2021-05-24 20:50:58 +0200

    Leaving "readline" dependencie

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
-const events = require('events');
-const emiter = new events.EventEmitter();
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const emiter = new (class extends require('events').EventEmitter {
+    constructor() {
+        super();
+        this.ask = ask;
+        this.setDefaultOptions = setDefaultOptions;
+        this.getDefaultOptions = getDefaultOptions;
+        this.getCustomizedOptions = getCustomizedOptions;
+        this.restartOptions = restartOptions;
+    }
 });
 const defaultOpts = {
     after: '\n',
     before: '',
-    limit: null,
-
+    limit: undefined,
 
     callback: () => { },
 };
-
+const stdin = process.openStdin();
 //the default options to be customized
 var customizedDefaultOptions = defaultOpts;
 
 //the function to export
 function ask(question, receivedOpts = {}) {
+    return new Promise((resolve, reject) => {
 
-    if(receivedOpts.constructor !== {}.constructor) throw new Error('Options has to be an object');
-    var opts = Object.assign({}, customizedDefaultOptions, receivedOpts);
+        if (receivedOpts.constructor !== {}.constructor) reject(new Error('Options have to be an object')); // options isn't an object ({})
+        var opts = Object.assign({}, customizedDefaultOptions, receivedOpts); // assign optins to opts
 
-    return new Promise((resolve) => {
         //the timeout to clear when answered (opt: limit)
 
-        //make the question
-        var finished = false;
-        rl.question(opts.before + question + opts.after, (answer) => {
-            if (!finished) {
-                opts.callback(answer);
-                resolve(answer);
-                finished = true;
-            }
-        });
+        //make the question 
+        process.stdout.write(opts.before + String(question) + opts.after);
 
+        //making the string
+        var string = '';
+        function onPress(chunk, key) {
+            console.log(chunk);
+            if (key.sequence === '\r') {
+                if (timeout) clearTimeout(timeout);
+                opts.callback(string);
+                stdin.removeListener(onPress);
+                resolve(string);
+                return;
+            }
+            string += key.sequence;
+        }
+
+        stdin.addListener('data', onPress);
+        stdin.addListener('data', c => {console.log(c)});
+
+
+
+        var timeout = undefined;
         //if opts.limit is defined and it is number:
-        if (typeof opts.limit == 'number') {
+        if ((typeof opts.limit) == 'number') {
             //abort questions if time is exceed
             //and save it in timeout to clear it whe question answered
-            setTimeout(() => {
-                if (!finished) {
-                    opts.callback(null);
-                    resolve(null);
-                    finished = true;
-                }
+            timeout = setTimeout(() => {
+                opts.callback(null);
+                resolve(null);
+                stdin.removeListener(onPress);
             }, opts.limit);
+        } else if (opts.limit !== undefined) {
+            reject(new Error('options.limit isn\'t a number'));
         }
     });
 }
@@ -63,13 +78,21 @@ background();
 
 //set default options
 function setDefaultOptions(options) {
-    customizedDefaultOptions = options;
+    Object.assign(customizedDefaultOptions, options);
+    return options;
+}
+
+function getDefaultOptions() {
+    return defaultOpts;
+}
+function getCustomizedOptions() {
+    return customizedDefaultOptions;
+}
+function restartOptions() {
+    setDefaultOptions(getDefaultOptions());
 }
 
 
 //export
-module.exports = {
-    ask,
-    emiter: emiter,
-    setDefaultOptions,
-};
+module.exports = emiter;
+ask('a').then(a => console.log(a));
