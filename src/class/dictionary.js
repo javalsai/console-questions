38e010d341fd3ece17937a21f1957f72f62759e9
javalsai <80113062+javalsai@users.noreply.github.com>
2021-08-15 15:50:54 +0200

    fix: add correct support for avaliable chars

--- src/class/dictionary.js
+++ src/class/dictionary.js
@@ -1,5 +1,6 @@
 let data = [];
 let cursor_index = 0;
+
 module.exports = function disctionary(buffer = Buffer.alloc(0)) {
     // Enter
     if (buffer[0] == 0x0d) {
@@ -26,17 +27,26 @@ module.exports = function disctionary(buffer = Buffer.alloc(0)) {
 
     // Backspace
     else if (buffer[0] === 0x08) {
-        data = data.slice(0, cursor_index - 1).concat(data.slice(cursor_index));
-        cursor_index--;
+        if (cursor_index !== 0) {
+            data = data.slice(0, cursor_index - 1).concat(data.slice(cursor_index));
+            cursor_index--;
+        }
     }
 
     // Arrow keys (left & right)
-    else if (buffer[0] === 0x1b && buffer[1] === 0x5b && (buffer[2] === 0x43 || buffer[2] === 0x44)) {
+    else if (buffer[0] === 0x1b && buffer[1] === 0x5b && (buffer[2] === 0x43 || buffer[2] === 0x44) && buffer[3] !== 0x7e) {
         buffer[2] === 0x43 ? (cursor_index !== data.join('').length ? cursor_index++ : null) : (cursor_index !== 0 ? cursor_index-- : null);
     }
 
+    // 'Del', 'Start', 'End'
+    else if (buffer[0] === 0x1b && buffer[1] === 0x5b && (buffer[2] === 0x31 || buffer[2] === 0x33 || buffer[2] === 0x34) && buffer[3] === 0x7e) {
+        if (buffer[2] === 0x33) data = data.slice(0, cursor_index).concat(data.slice(cursor_index + 1));
+        else cursor_index = data.join('').length * (buffer[2] === 0x34);
+    }
+
     return {
-        print: data.join('') + '\x1B[D'.repeat(data.join('').length) + '\x1B[C'.repeat(cursor_index),
+        print: data.join(''),
+        cursor_index: cursor_index,
         resolve: data.join('')
     };
 }
--- src/events_manager.js
+++ src/events_manager.js
@@ -7,12 +7,14 @@ _.stdin.on('data', (chunk) => {
 });
 
 let last_printed_length = 0;
+let last_cursor_index = 0;
 _.instance.on('_keypress', (chunk) => {
     let to_print = _.instance.dictionary(chunk);
     switch (_.instance.mode) {
         case 0:
-            process.stdout.write('\b \b'.repeat(last_printed_length) + to_print.print);
+            process.stdout.write('\b'.repeat(last_cursor_index) + ' \b'.repeat(last_printed_length).split('').sort().reverse().join('') + to_print.print + '\x1B[D'.repeat(to_print.print.length) + '\x1B[C'.repeat(to_print.cursor_index));
             last_printed_length = to_print.print.length;
+            last_cursor_index = to_print.cursor_index;
             break;
         case 1:
             break;
